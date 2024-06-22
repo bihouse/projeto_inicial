48d13e9913606e51779bf8e4d3a7338ee96117d2
Davi <fsales.davi@gmail.com>
2024-06-22 18:23:16 -0300

    meu primeiro commit

---
+++ inciando.py
@@ -0,0 +1,19 @@
+print("jornada de dados")
+
+#Variaveis
+idade = 35
+nome = "Davi"
+
+#Print
+print(idade)
+print(nome)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#lista
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse()
